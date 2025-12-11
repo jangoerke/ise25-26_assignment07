93a56e16471ecb80e15fcafd5ceb8047e3875af8
Jan Görke <jangoerke@proton.me>
2025-12-11 12:04:48 +0100

    implemented missing methods in ReviewController.java Error wegen .fromDomain(List<Review>)

--- api/src/main/java/de/seuhd/campuscoffee/api/controller/ReviewController.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/controller/ReviewController.java
@@ -1,6 +1,8 @@
 package de.seuhd.campuscoffee.api.controller;
 
+import de.seuhd.campuscoffee.api.dtos.PosDto;
 import de.seuhd.campuscoffee.api.dtos.ReviewDto;
+import de.seuhd.campuscoffee.api.dtos.UserDto;
 import de.seuhd.campuscoffee.api.mapper.DtoMapper;
 import de.seuhd.campuscoffee.api.mapper.ReviewDtoMapper;
 import de.seuhd.campuscoffee.api.openapi.CrudOperation;
@@ -8,7 +10,9 @@ import de.seuhd.campuscoffee.domain.model.objects.Review;
 import de.seuhd.campuscoffee.domain.ports.api.CrudService;
 import de.seuhd.campuscoffee.domain.ports.api.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
@@ -54,4 +58,65 @@ public class ReviewController extends CrudController<Review, ReviewDto, Long> {
     }
 
     // TODO: Implement the missing methods/endpoints.
+
+    @Operation
+    @CrudOperation(operation=GET_BY_ID, resource=REVIEW)
+    @GetMapping("/{id}")
+    public @NonNull ResponseEntity < ReviewDto > getById (
+            @Parameter(description="Unique identifier of the reviev to retrieve.", required=true)
+            @PathVariable Long id) {
+        return super.getById(id);
+    }
+
+    @Operation
+    @CrudOperation(operation=CREATE, resource=REVIEW)
+    @PostMapping("")
+    public @NonNull ResponseEntity<ReviewDto> create(
+            @Parameter(description="Data of the Review to create.", required=true)
+            @RequestBody @Valid ReviewDto reviewDto) {
+        return super.create(reviewDto);
+    }
+
+    @Operation
+    @CrudOperation(operation=UPDATE, resource = REVIEW)
+    @PutMapping("/{id}")
+    public @NonNull ResponseEntity<ReviewDto> update(
+            @Parameter(description="Unique identifier of the Review to update.", required=true)
+            @PathVariable Long id,
+            @Parameter(description="Data of the Review to update.", required=true)
+            @RequestBody @Valid ReviewDto reviewDto) {
+        return super.update(id, reviewDto);
+    }
+
+    @Operation
+    @CrudOperation(operation=DELETE, resource=REVIEW)
+    @DeleteMapping("/{id}")
+    public @NonNull ResponseEntity < Void > delete (
+        @Parameter(description="Unique identifier of the Review to delete.", required=true)
+        @PathVariable Long id
+    ) {
+        return super.delete(id);
+    }
+
+    @Operation
+    @CrudOperation(operation=FILTER, resource=REVIEW)
+    @GetMapping("/filter")
+    public ResponseEntity < List < ReviewDto > > filter (
+        @Parameter(description = "ID to be retrieved", required = true)
+        @RequestParam ( " pos_id " ) Long posId ,
+        @RequestParam ( " approved " ) Boolean approved
+    ) {
+        return ResponseEntity.ok(
+                reviewDtoMapper.fromDomain(reviewService.filter(posId, approved))
+        );
+    }
+
+    public ResponseEntity < ReviewDto > approve (
+    @PathVariable Long id ,
+    @RequestParam ( " user_id " ) Long userId
+    ) {
+        return ResponseEntity.ok(
+            reviewDtoMapper.fromDomain(reviewService.approve(reviewService.getById(id), userId))
+        );
+    }
 }
